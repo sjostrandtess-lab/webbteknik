890b9575914500b89f35d6aa376c360bf38c2a51
tess sjostrand <id23tsd@cs.umu.se>
2025-09-04 15:29:27 +0200

    tr

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-console.log("JavaScript-filen är laddad!");
-
 function myFunction(){
 	let x = document.getElementById("fname");
 	x.value = x.value.toUpperCase();
+}
+
+function mOver(obj){
+	obj.innerHTML = "Bear Croft"
+}
+
+function mOut(obj){
+	obj.innerHTML = "Välkommen till vår kennel"
 }
