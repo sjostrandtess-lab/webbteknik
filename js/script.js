aa996bfa4d2742b8fbd38ef69aef2aa1d8806236
tess sjostrand <id23tsd@cs.umu.se>
2025-09-04 16:35:33 +0200

    d

--- js/script.js
+++ js/script.js
@@ -12,16 +12,14 @@ function mOut(obj){
 }
 
 $(function() {
-    $(".answer").hide(); // Göm svaren från början
+    $(".answer").hide();
 
     $(".question").click(function() {
         let svar = $(this).next(".answer");
 
         if (svar.is(":visible")) {
-            // Om svaret redan är synligt, tona och slajda bort det
             svar.fadeTo(200, 0.2).slideUp(300);
         } else {
-            // Visa det med slideDown och öka opaciteten
             svar.slideDown(300).fadeTo(200, 1);
         }
     });
