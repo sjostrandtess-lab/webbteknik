93a195977f8cb62606e8627b68c29108d8a76dd1
tess sjostrand <id23tsd@cs.umu.se>
2025-09-04 16:31:33 +0200

    yeeee

--- js/script.js
+++ js/script.js
@@ -10,3 +10,19 @@ function mOver(obj){
 function mOut(obj){
 	obj.innerHTML = "Välkommen till vår kennel"
 }
+
+$(function() {
+    $(".answer").hide(); // Göm svaren från början
+
+    $(".question").click(function() {
+        let svar = $(this).next(".answer");
+
+        if (svar.is(":visible")) {
+            // Om svaret redan är synligt, tona och slajda bort det
+            svar.fadeTo(200, 0.2).slideUp(300);
+        } else {
+            // Visa det med slideDown och öka opaciteten
+            svar.slideDown(300).fadeTo(200, 1);
+        }
+    });
+});
